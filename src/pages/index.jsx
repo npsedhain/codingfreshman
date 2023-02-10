50bd952aed9552b16a930d480590a14499f3f1b3
npsedhain <npsedhain@gmail.com>
2023-02-10 22:53:29 +0545

    Add newsletter subscription form.

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -23,6 +23,7 @@ import logoPolydelic from '@/images/logos/polydelic_logo.svg'
 import { generateRssFeed } from '@/lib/generateRssFeed'
 import { getAllArticles } from '@/lib/getAllArticles'
 import { formatDate } from '@/lib/formatDate'
+import { useState } from 'react'
 
 function MailIcon(props) {
   return (
@@ -107,9 +108,35 @@ function SocialLink({ icon: Icon, ...props }) {
 }
 
 function Newsletter() {
+  const [email, setEmail] = useState('')
+  const [name, setName] = useState('')
+  const [isSubscribed, setIsSubscribed] = useState(false)
+
+  const onSubmit = async (e) => {
+    e.preventDefault()
+
+    const res = await fetch(
+      `${process.env.NEXT_PUBLIC_CONVERT_KIT_BASE_URL}/forms/${process.env.NEXT_PUBLIC_CONVERT_KIT_FORM_ID}/subscribe`,
+      {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({
+          email,
+          first_name: name,
+          api_key: process.env.NEXT_PUBLIC_CONVERT_KIT_API,
+        }),
+      }
+    )
+
+    const body = await res.json()
+    if (body.subscription) setIsSubscribed(true)
+  }
+
   return (
     <form
-      action="/thank-you"
+      onSubmit={onSubmit}
       className="rounded-2xl border border-zinc-100 p-6 dark:border-zinc-700/40"
     >
       <h2 className="flex text-sm font-semibold text-zinc-900 dark:text-zinc-100">
@@ -119,18 +146,39 @@ function Newsletter() {
       <p className="mt-2 text-sm text-zinc-600 dark:text-zinc-400">
         Get notified when I publish something new, and unsubscribe at any time.
       </p>
-      <div className="mt-6 flex">
-        <input
-          type="email"
-          placeholder="Email address"
-          aria-label="Email address"
-          required
-          className="min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
-        />
-        <Button type="submit" className="ml-4 flex-none">
-          Join
-        </Button>
-      </div>
+      {isSubscribed ? (
+        <p className="mt-6 text-sm text-zinc-600 dark:text-zinc-400">
+          Thank you for subscribing. We will have a lot of fun together and I
+          promise to enlighten you every Sunday!
+        </p>
+      ) : (
+        <div className="mt-6">
+          <div className="flex">
+            <input
+              type="text"
+              placeholder="First name"
+              aria-label="First name"
+              required
+              onChange={(e) => setName(e.target.value)}
+              className="mb-3 w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+            />
+            <Button className="invisible ml-4">Join</Button>
+          </div>
+          <div className="flex">
+            <input
+              type="email"
+              placeholder="Email address"
+              aria-label="Email address"
+              required
+              onChange={(e) => setEmail(e.target.value)}
+              className="w-full min-w-0 flex-auto appearance-none rounded-md border border-zinc-900/10 bg-white px-3 py-[calc(theme(spacing.2)-1px)] shadow-md shadow-zinc-800/5 placeholder:text-zinc-400 focus:border-teal-500 focus:outline-none focus:ring-4 focus:ring-teal-500/10 dark:border-zinc-700 dark:bg-zinc-700/[0.15] dark:text-zinc-200 dark:placeholder:text-zinc-500 dark:focus:border-teal-400 dark:focus:ring-teal-400/10 sm:text-sm"
+            />
+            <Button type="submit" className="ml-4">
+              Join
+            </Button>
+          </div>
+        </div>
+      )}
     </form>
   )
 }
@@ -254,7 +302,8 @@ export default function Home({ articles }) {
             I tell stories
           </h1>
           <p className="mt-6 text-base text-zinc-600 dark:text-zinc-400">
-            I’m Anup, a software engineer and storyteller based in Nepal. I am sharing what I learn in my journey as a software developer.
+            I’m Anup, a software engineer and storyteller based in Nepal. I am
+            sharing what I learn in my journey as a software developer.
           </p>
           <div className="mt-6 flex gap-6">
             <SocialLink
@@ -289,7 +338,7 @@ export default function Home({ articles }) {
             ))}
           </div>
           <div className="space-y-10 lg:pl-16 xl:pl-24">
-            {/* <Newsletter /> TODO */}
+            <Newsletter />
             <Resume />
           </div>
         </div>
